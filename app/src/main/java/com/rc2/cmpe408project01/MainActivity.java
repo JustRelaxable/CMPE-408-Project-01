7cc3709cd27e08ba1683b63ab374c4d1e55742a1
Taha Sökmen <resimli1000@gmail.com>
2022-11-08 12:36:02 +0300

    Project files created

---
+++ app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rc2.cmpe408project01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
