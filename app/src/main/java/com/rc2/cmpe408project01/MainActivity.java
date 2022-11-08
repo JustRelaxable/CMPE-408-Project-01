369758c3f47cef7e8a172d4f4b7c87abc7b0f080
Taha Sökmen <resimli1000@gmail.com>
2022-11-08 14:19:24 +0300

    Submit button functionality partially implemented

--- app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
+++ app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     String[] facultyList = {"Faculty of Arts","Faculty of Law","Faculty of Engineering"};
 
@@ -19,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupViews();
         loadFacultySpinner();
         handleStudentIDTextChangedListener();
+    }
+
+    private void setupViews() {
+        Button submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(this);
     }
 
     private void handleStudentIDTextChangedListener() {
@@ -49,5 +57,22 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item,facultyList);
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         facultySpinner.setAdapter(ad);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.submit_button:
+                //TODO: Implement control mechanism to all defined widgets to check if fields are complete.
+                boolean allFieldsFilled = false;
+                if(allFieldsFilled)
+                    Toast.makeText(this, R.string.fields_submitted_successfully, Toast.LENGTH_SHORT).show();
+                    //TODO: Implement and show TextView, fill it with submitted information and display to the user.
+                else
+                    Toast.makeText(this, R.string.fields_are_incomplete_warning, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
