749618d6191487e7433de2a47eb983661b86ed38
Taha Sökmen <resimli1000@gmail.com>
2022-11-08 14:04:27 +0300

    StudentID TextEdit lenght fixed to 11 characters

--- app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
+++ app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
@@ -3,8 +3,12 @@ package com.rc2.cmpe408project01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         loadFacultySpinner();
+        handleStudentIDTextChangedListener();
+    }
+
+    private void handleStudentIDTextChangedListener() {
+        EditText studentIDText = findViewById(R.id.student_id);
+        studentIDText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editable.length() > 11){
+                    editable = editable.delete(11,12);
+                }
+            }
+        });
     }
 
     private void loadFacultySpinner() {
