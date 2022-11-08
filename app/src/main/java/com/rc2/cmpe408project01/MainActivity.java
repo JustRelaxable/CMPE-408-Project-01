ff00205234ccc4cb7e290c618d4011c79aa1957e
Taha Sökmen <resimli1000@gmail.com>
2022-11-08 13:42:09 +0300

    Some layout elements and functionality implemented

--- app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
+++ app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
@@ -3,12 +3,25 @@ package com.rc2.cmpe408project01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] facultyList = {"Faculty of Arts","Faculty of Law","Faculty of Engineering"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFacultySpinner();
+    }
+
+    private void loadFacultySpinner() {
+        Spinner facultySpinner = findViewById(R.id.faculty_spinner);
+        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item,facultyList);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        facultySpinner.setAdapter(ad);
     }
 }
