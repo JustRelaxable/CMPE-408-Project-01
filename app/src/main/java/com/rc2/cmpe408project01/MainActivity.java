36a99b635e88b8fa8a49a73bc89d475b18b67bda
Taha Sökmen <resimli1000@gmail.com>
2022-11-14 13:56:05 +0300

    Code refactoring

--- app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
+++ app/src/main/java/com/rc2/cmpe408project01/MainActivity.java
@@ -1,9 +1,7 @@
 package com.rc2.cmpe408project01;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,13 +9,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -41,20 +41,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RadioButton full_scholarship;
     private RadioButton half_scholarship;
     private RadioButton none_scholarship;
-    private AlertDialog dialog;
-    private AlertDialog.Builder dialogBuilder;
-    private TextView pop_name;
-    private TextView pop_Lname;
-    private TextView pop_id;
-    private TextView pop_gender;
-    private TextView pop_scholar;
-    private TextView pop_bp;
-    private TextView pop_bd;
-    private TextView pop_gpa;
     private TextView birthDateText;
-    private String city_value;
-    private String bd_values;
-    private String faculty_value;
+    private TextView cityName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             InputStream is = getAssets().open("cities.json");
             int size = is.available();
             byte[] buffer = new byte[size];
-            is.read(buffer);
+            int result = is.read(buffer);
             is.close();
             String raw = new String(buffer, StandardCharsets.UTF_8);
             cities = new JSONObject(raw);
@@ -128,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 1; i <= 81; i++) {
             cityCodes[i - 1] = String.valueOf(i);
         }
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, cityCodes);
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cityCodes);
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         birthplaceSpinner.setAdapter(ad);
         TextView cityName = findViewById(R.id.city_name);
@@ -137,7 +125,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 try {
                     cityName.setText(cities.getString(String.valueOf(i + 1)));
-                    city_value = (cities.getString(String.valueOf(i + 1)));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -162,6 +149,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button birtDateButton = findViewById(R.id.birth_date_button);
         birtDateButton.setOnClickListener(this);
+
+        full_scholarship = findViewById(R.id.full);
+        half_scholarship = findViewById(R.id.half);
+        none_scholarship = findViewById(R.id.none);
+
+        name = findViewById(R.id.name_id);
+        last_name = findViewById(R.id.last_name);
+        studentIDText = findViewById(R.id.student_id);
+        gpa = findViewById(R.id.gpa_id);
+        sex_male = findViewById(R.id.male);
+        sex_female = findViewById(R.id.female);
+
+        cityName = findViewById(R.id.city_name);
     }
 
     private void handleStudentIDTextChangedListener() {
@@ -217,78 +217,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //TODO: Implement control mechanism to all defined widgets to check if fields are complete.
                 boolean allFieldsFilled = false;
                 if (allFieldsFilled || true) {   // geçici oalrak true
-                    name = findViewById(R.id.name_id);
-                    String name_value = name.getText().toString();
-
-                    last_name = findViewById(R.id.last_name);
-                    String Lname_value = last_name.getText().toString();
-
-                    studentIDText = findViewById(R.id.student_id);
-                    String id_value = studentIDText.getText().toString();
-
-                    gpa = findViewById(R.id.gpa_id);
-                    String gpa_value = gpa.getText().toString();
-
-
-                    sex_male = findViewById(R.id.male);
-                    sex_female = findViewById(R.id.female);
-                    String sex_value = "none";
-
-                    if (sex_male.isChecked()) {
-                        sex_value = "male";
-                    } else if (sex_female.isChecked()) {
-                        sex_value = "female";
-                    }
-
-                    full_scholarship = findViewById(R.id.full);
-                    half_scholarship = findViewById(R.id.half);
-                    none_scholarship = findViewById(R.id.none);
-                    String scholarship_value = "none";
-
-                    if (full_scholarship.isChecked()) {
-                        scholarship_value = "full";
-                    } else if (half_scholarship.isChecked()) {
-                        scholarship_value = "half";
-                    } else if (none_scholarship.isChecked()) {
-                        scholarship_value = "none";
-                    }
-
-
-                    CreateNewDialog(name_value, Lname_value, id_value, sex_value, scholarship_value, city_value, gpa_value, bd_values);
-
+                    CreateStudentOutputDialog();
                 } else
                     Toast.makeText(this, R.string.fields_are_incomplete_warning, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.reset_button:
-
-                id = findViewById(R.id.student_id);
                 id.setText("");
-
-                name = findViewById(R.id.name_id);
                 name.setText("");
-
-                last_name = findViewById(R.id.last_name);
                 last_name.setText("");
-
-                gpa = findViewById(R.id.gpa_id);
                 gpa.setText("");
-
-                sex_male = findViewById(R.id.male);
                 sex_male.setChecked(false);
-
-                sex_female = findViewById(R.id.female);
                 sex_female.setChecked(false);
-
-                full_scholarship = findViewById(R.id.full);
                 full_scholarship.setChecked(false);
-                half_scholarship = findViewById(R.id.half);
                 half_scholarship.setChecked(false);
-                none_scholarship = findViewById(R.id.none);
                 none_scholarship.setChecked(false);
-
                 birthDateText.setText("Birth date is not selected!");
-
-
                 break;
             case R.id.exit_button:
                 System.exit(0);
@@ -299,7 +242,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                         birthDateText = findViewById(R.id.birth_date_text);
                         birthDateText.setText(day + "/" + month + "/" + year);
-                        bd_values = (day + "/" + month + "/" + year);
                     }
                 }, 2020, 1, 1).show();
                 break;
@@ -309,37 +251,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public void CreateNewDialog(String name, String last_name, String id, String sex, String scholarship, String bp, String gpa, String bd) {
-        dialogBuilder = new AlertDialog.Builder(this);
+    public void CreateStudentOutputDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         final View contactPopupView = getLayoutInflater().inflate(R.layout.popup, null);
 
         dialogBuilder.setView(contactPopupView);
-        dialog = dialogBuilder.create();
+        Dialog dialog = dialogBuilder.create();
         dialog.show();
 
-        pop_id = contactPopupView.findViewById(R.id.pop_id);
-        pop_id.setText(id);
+        String name_value = name.getText().toString();
+        String Lname_value = last_name.getText().toString();
+        String id_value = studentIDText.getText().toString();
+        String gpa_value = gpa.getText().toString();
+        sex_male = findViewById(R.id.male);
+        sex_female = findViewById(R.id.female);
 
-        pop_name = contactPopupView.findViewById(R.id.pop_name);
-        pop_name.setText(name);
+        String sex_value = "none";
 
-        pop_Lname = contactPopupView.findViewById(R.id.pop_Lname);
-        pop_Lname.setText(last_name);
+        if (sex_male.isChecked()) {
+            sex_value = "male";
+        } else if (sex_female.isChecked()) {
+            sex_value = "female";
+        }
 
-        pop_gender = contactPopupView.findViewById(R.id.pop_gender);
-        pop_gender.setText(sex);
 
-        pop_scholar = contactPopupView.findViewById(R.id.pop_scholar);
-        pop_scholar.setText(scholarship);
+        String scholarship_value = "none";
 
-        pop_bp = contactPopupView.findViewById(R.id.pop_bp);
-        pop_bp.setText(bp);
+        if (full_scholarship.isChecked()) {
+            scholarship_value = "full";
+        } else if (half_scholarship.isChecked()) {
+            scholarship_value = "half";
+        } else if (none_scholarship.isChecked()) {
+            scholarship_value = "none";
+        }
 
-        pop_gpa = contactPopupView.findViewById(R.id.pop_gpa);
-        pop_gpa.setText(gpa);
+        TextView pop_id = contactPopupView.findViewById(R.id.pop_id);
+        pop_id.setText(id_value);
 
-        pop_bd = contactPopupView.findViewById(R.id.pop_bd);
-        pop_bd.setText(bd);
+        TextView pop_name = contactPopupView.findViewById(R.id.pop_name);
+        pop_name.setText(name_value);
+
+        TextView pop_Lname = contactPopupView.findViewById(R.id.pop_Lname);
+        pop_Lname.setText(Lname_value);
+
+        TextView pop_gender = contactPopupView.findViewById(R.id.pop_gender);
+        pop_gender.setText(sex_value);
+
+        TextView pop_scholar = contactPopupView.findViewById(R.id.pop_scholar);
+        pop_scholar.setText(scholarship_value);
+
+        TextView pop_bp = contactPopupView.findViewById(R.id.pop_bp);
+        pop_bp.setText(cityName.getText());
+
+        TextView pop_gpa = contactPopupView.findViewById(R.id.pop_gpa);
+        pop_gpa.setText(gpa_value);
+
+        TextView pop_bd = contactPopupView.findViewById(R.id.pop_bd);
+        pop_bd.setText(birthDateText.getText());
 
         TextView pop_faculty = contactPopupView.findViewById(R.id.pop_faculty);
         Spinner facultySpinner = findViewById(R.id.faculty_spinner);
